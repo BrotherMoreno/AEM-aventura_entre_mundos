8c35080903ff60c7abb67d3d2c0e465a44ed3853
BrotherMoreno <pablomorecord@gmail.com>
2025-05-11 20:14:22 -0500

    se termino nivel 1 y nivel 2, nivel 3 a medias se añadieron enemigos y la clase deborador

--- GAME_PARCIAL/jugador.cpp
+++ GAME_PARCIAL/jugador.cpp
@@ -8,26 +8,28 @@ void Jugador::gotoxy(int x, int y) {
 }
 void Jugador::mover(int tecla, int jugador, int mapa1[30][120]) {
     if (!viva) return;
+    if (jugador == 1) {
 
-    int nuevaPosX = posX;
-    int nuevaPosY = posY;
+        int nuevaPosX = posX;
+        int nuevaPosY = posY;
 
-    // Calcula la nueva posición basada en la tecla presionada
-    if (tecla == 72 && posY > 0) nuevaPosY--; // Arriba
-    else if (tecla == 80 && posY < 29) nuevaPosY++; // Abajo (ajustado para 30 filas)
-    else if (tecla == 75 && posX > 0) nuevaPosX--; // Izquierda
-    else if (tecla == 77 && posX < 119) nuevaPosX++; // Derecha (ajustado para 120 columnas)
+        // Calcula la nueva posición basada en la tecla presionada
+        if (tecla == 72 && posY > 0) nuevaPosY--; // Arriba
+        else if (tecla == 80 && posY < 29) nuevaPosY++; // Abajo (ajustado para 30 filas)
+        else if (tecla == 75 && posX > 0) nuevaPosX--; // Izquierda
+        else if (tecla == 77 && posX < 119) nuevaPosX++; // Derecha (ajustado para 120 columnas)
 
-    // Verifica si la nueva posición es válida en el mapa
-    if (/*mapa1[nuevaPosY][nuevaPosX] == 3&& mapa1[nuevaPosY][nuevaPosX+1] == 3&& mapa1[nuevaPosY][nuevaPosX+2] == 3&& mapa1[nuevaPosY][nuevaPosX+3] == 3&& mapa1[nuevaPosY][nuevaPosX+4] == 3 &&*/
-        mapa1[nuevaPosY+1][nuevaPosX] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 1] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 2] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 3] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 4] == 3 && 
-        mapa1[nuevaPosY + 2][nuevaPosX] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 4] == 3 &&
-        mapa1[nuevaPosY + 3][nuevaPosX] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 4] == 3 && 
-        mapa1[nuevaPosY + 4][nuevaPosX] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 4] == 3 ) {
-        borrar(jugador); // Borra la posición anterior
-        posX = nuevaPosX;
-        posY = nuevaPosY;
-        dibujar(jugador); // Dibuja en la nueva posición
+        // Verifica si la nueva posición es válida en el mapa
+        if (/*mapa1[nuevaPosY][nuevaPosX] == 3&& mapa1[nuevaPosY][nuevaPosX+1] == 3&& mapa1[nuevaPosY][nuevaPosX+2] == 3&& mapa1[nuevaPosY][nuevaPosX+3] == 3&& mapa1[nuevaPosY][nuevaPosX+4] == 3 &&*/
+            mapa1[nuevaPosY+1][nuevaPosX] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 1] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 2] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 3] == 3 && mapa1[nuevaPosY+1][nuevaPosX + 4] == 3 && 
+            mapa1[nuevaPosY + 2][nuevaPosX] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 2][nuevaPosX + 4] == 3 &&
+            mapa1[nuevaPosY + 3][nuevaPosX] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 3][nuevaPosX + 4] == 3 && 
+            mapa1[nuevaPosY + 4][nuevaPosX] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 1] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 2] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 3] == 3 && mapa1[nuevaPosY + 4][nuevaPosX + 4] == 3 ) {
+            borrar(jugador); // Borra la posición anterior
+            posX = nuevaPosX;
+            posY = nuevaPosY;
+            dibujar(jugador); // Dibuja en la nueva posición
+        }
     }
 
     if (jugador == 2) {
@@ -37,6 +39,38 @@ void Jugador::mover(int tecla, int jugador, int mapa1[30][120]) {
         else if (tecla == 'a' && posX > 0) posX -= velocidad;
         else if (tecla == 'd' && posX < MAX_WIDTH - 3) posX += velocidad;
         dibujar(2);
+    }
+    if (jugador == 3) {
+        int nuevaPosX = posX;
+        int nuevaPosY = posY;
+
+        // Calcula la nueva posición basada en la tecla presionada
+        if (tecla == 72 && posY > 0) nuevaPosY--; // Arriba
+        else if (tecla == 80 && posY < 29) nuevaPosY++; // Abajo (ajustado para 30 filas)
+        else if (tecla == 75 && posX > 0) nuevaPosX--; // Izquierda
+        else if (tecla == 77 && posX < 119) nuevaPosX++; // Derecha (ajustado para 120 columnas)
+
+        // Verifica si la nueva posición es válida en el mapa
+        if ((mapa1[nuevaPosY][nuevaPosX] == 6&& mapa1[nuevaPosY][nuevaPosX+1] == 6&& mapa1[nuevaPosY][nuevaPosX+2] == 6&& mapa1[nuevaPosY][nuevaPosX+3] == 6&& mapa1[nuevaPosY][nuevaPosX+4] == 6&&
+            mapa1[nuevaPosY + 1][nuevaPosX] == 6 && mapa1[nuevaPosY + 1][nuevaPosX + 1] == 6&& mapa1[nuevaPosY + 1][nuevaPosX + 2] == 6 && mapa1[nuevaPosY + 1][nuevaPosX + 3] == 6 && mapa1[nuevaPosY + 1][nuevaPosX + 4] == 6 &&
+            mapa1[nuevaPosY + 2][nuevaPosX] == 6 && mapa1[nuevaPosY + 2][nuevaPosX + 1] == 6 && mapa1[nuevaPosY + 2][nuevaPosX + 2] == 6 && mapa1[nuevaPosY + 2][nuevaPosX + 3] == 6 && mapa1[nuevaPosY + 2][nuevaPosX + 4] == 6 &&
+            mapa1[nuevaPosY + 3][nuevaPosX] == 6 && mapa1[nuevaPosY + 3][nuevaPosX + 1] == 6 && mapa1[nuevaPosY + 3][nuevaPosX + 2] == 6 && mapa1[nuevaPosY + 3][nuevaPosX + 3] == 6 && mapa1[nuevaPosY + 3][nuevaPosX + 4] == 6 )||(
+            mapa1[nuevaPosY][nuevaPosX] == 5 || mapa1[nuevaPosY][nuevaPosX + 1] == 5 || mapa1[nuevaPosY][nuevaPosX + 2] == 5 ||mapa1[nuevaPosY][nuevaPosX + 3] == 5 || mapa1[nuevaPosY][nuevaPosX + 4] == 5 &&
+            mapa1[nuevaPosY + 1][nuevaPosX] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 1] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 2] == 5||mapa1[nuevaPosY + 1][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 4] == 5 ||
+            mapa1[nuevaPosY + 2][nuevaPosX] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 1] == 5 ||mapa1[nuevaPosY + 2][nuevaPosX + 2] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 4] == 5 ||
+            mapa1[nuevaPosY + 3][nuevaPosX] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 1] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 2] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 4] == 5)) {
+            borrar(jugador); // Borra la posición anterior
+            posX = nuevaPosX;
+            posY = nuevaPosY;
+            dibujar(jugador); // Dibuja en la nueva posición
+        }
+        if ((
+            mapa1[nuevaPosY][nuevaPosX] == 5 || mapa1[nuevaPosY][nuevaPosX + 1] == 5 || mapa1[nuevaPosY][nuevaPosX + 2] == 5 || mapa1[nuevaPosY][nuevaPosX + 3] == 5 || mapa1[nuevaPosY][nuevaPosX + 4] == 5 &&
+            mapa1[nuevaPosY + 1][nuevaPosX] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 1] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 2] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 1][nuevaPosX + 4] == 5 ||
+            mapa1[nuevaPosY + 2][nuevaPosX] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 1] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 2] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 2][nuevaPosX + 4] == 5 ||
+            mapa1[nuevaPosY + 3][nuevaPosX] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 1] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 2] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 3] == 5 || mapa1[nuevaPosY + 3][nuevaPosX + 4] == 5)) {
+
+        }
     }
 }
 
@@ -81,6 +115,8 @@ void Jugador::dibujar(int elegirDibujoJugador) {
                 }
             }
         }
+        mostrarVida();
+
     }
     if (elegirDibujoJugador == 2) {
 
@@ -101,9 +137,48 @@ void Jugador::dibujar(int elegirDibujoJugador) {
                     std::cout << block;
                 }
             }
+            mostrarVida();
+
         }
     }
-    mostrarVida();
+    if (elegirDibujoJugador == 3) {
+        int forma[4][5] = {
+            {0, 5, 5, 5, 0}, // Cabeza
+            {1, 4, 4, 4, 1}, // Brazos
+            {3, 2, 2, 2, 3}, // Cuerpo
+            {0, 2, 0, 2, 0}  // Pies
+        };
+
+        for (int i = 0; i < 4; ++i) {
+            gotoxy(posX, posY + i);
+            for (int j = 0; j < 5; ++j) {
+                if (forma[i][j] == 0) {
+                    Console::ForegroundColor = ConsoleColor::Black; // Espacio vacío
+                    std::cout << " ";
+                }
+                if (forma[i][j] == 1) {
+                    Console::ForegroundColor = ConsoleColor::DarkRed; // Brazos
+                    std::cout << block;
+                }
+                if (forma[i][j] == 2) {
+                    Console::ForegroundColor = ConsoleColor::White; // Cabeza, cuerpo y pies
+                    std::cout << block;
+                }
+                if (forma[i][j] == 3) {
+                    Console::ForegroundColor = ConsoleColor::DarkGray; // Cabeza, cuerpo y pies
+                    std::cout << (char)223;
+                }
+                if (forma[i][j] == 4) {
+                    Console::ForegroundColor = ConsoleColor::DarkBlue; // Cabeza, cuerpo y pies
+                    std::cout << (char)223;
+                }
+                if (forma[i][j] == 5) {
+                    Console::ForegroundColor = ConsoleColor::Yellow; // Cabeza, cuerpo y pies
+                    std::cout << (char)219;
+                }
+            }
+        }
+    }
 }
 
 void Jugador::borrar(int jugador) {
@@ -119,6 +194,13 @@ void Jugador::borrar(int jugador) {
         for (int i = 0; i < 2; ++i) {
             gotoxy(posX, posY + i);
             std::cout << "  ";  // 11 espacios
+        }
+    }
+    if (jugador == 3) {
+
+        for (int i = 0; i < 4; ++i) {
+            gotoxy(posX, posY + i);
+            std::cout << "     ";  // 11 espacios
         }
     }
 
--- GAME_PARCIAL/mapas.h
+++ GAME_PARCIAL/mapas.h
@@ -15,8 +15,15 @@ public:
 	Mapas();
 	void dibujarMapa1();
 	void dibujarMapa2();
-
+	void dibujarMapa3();
+	void dibujarCasaMadera();
+	void dibujarCarretera();
+	void dibujarArbol();
+	int carretera[9][70];
+	int piscina[30][120];
+	int casaMadera[30][120];
 	int mapa1[30][120];
 	int mapa2[30][120];
+	int mapa3[30][120];
 };
 #endif // !MAPAS_H
